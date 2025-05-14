6e30a4083d25d309065ead79b3579df17cf0d84e
OlehShpunt <olehshpunt2@gmail.com>
2025-05-13 17:59:22 -0700

    Install and test Tailwind CSS

--- frontend/setup/src/main.js
+++ frontend/setup/src/main.js
@@ -1,8 +1,23 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import './style.css'
 
-// import './style.css'
+
 import App from './App.vue'
 import myCode from '../../src/text.vue'
-import CameraComponent from '../../src/cameraComponent.vue'
 
-createApp(CameraComponent).mount('#app')
+import CameraComponent from '../../src/cameraComponent.vue'
+import CameraView from './views/CameraView.vue'
+import CapturedImage from './views/CapturedImage.vue'
+
+const routes = [
+    { path: '/', name: 'CameraView', component: CameraView },
+    { path: '/captured', name: 'CapturedImage', component: CapturedImage }
+]
+
+export default createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+createApp(CameraView).mount('#app')
---
+++ frontend/setup/src/views/CapturedImage.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="captured-image">
+      <img :src="imageSrc" alt="Captured" />
+    </div>
+  </template>
+  
+  <script setup>
+  import { computed } from 'vue'
+  import { useRoute } from 'vue-router'
+  
+  const route = useRoute()
+  const imageSrc = computed(() => route.query.img)
+  </script>
+  
+  <style scoped>
+  .captured-image {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+  }
+  img {
+    max-width: 90%;
+    max-height: 90%;
+    border: 2px solid #000;
+    border-radius: 10px;
+  }
+  </style>
+  
